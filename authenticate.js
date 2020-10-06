9f3dc3d3c37ab9c008d721f0d6c1ac1162c7397d
Lance Hsu <lancehsu12@gmail.com>
2020-10-06 12:16:21 +0800

    Deploy setting

--- authenticate.js
+++ authenticate.js
@@ -1,34 +1,37 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
+const jwt = require("jsonwebtoken");
 // const FacebookTokenStrategy = require('passport-facebook-token');
 
-const User = require('./models/user');
-const config = require('./config');
+const User = require("./models/user");
+const config = require("./config");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 // get JWT by encoding user id followed by secret key
-exports.getToken = user => jwt.sign(user, config.secretKey, { expiresIn: '7d' });
+exports.getToken = (user) =>
+  jwt.sign(user, config.secretKey, { expiresIn: "7d" });
 
 const opts = {};
 // get JWT from bearer of Authentication header
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts, async (jwtPayload, done) => {
-  console.log('JWT payload: ', jwtPayload);
-  // strategy: find if there is a user with the input jwt payload
-  try {
-    const user = await User.findOne({ _id: jwtPayload.id });
-    return done(null, user);
-  } catch (err) {
-    return done(err, false);
-  }
-}));
+exports.jwtPassport = passport.use(
+  new JwtStrategy(opts, async (jwtPayload, done) => {
+    console.log("JWT payload: ", jwtPayload);
+    // strategy: find if there is a user with the input jwt payload
+    try {
+      const user = await User.findOne({ _id: jwtPayload.id });
+      return done(null, user);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
 
 // exports.facebookPassport = passport.use(new FacebookTokenStrategy({
 //   clientID: config.facebook.clientId, clientSecret: config.facebook.clientSecret,
@@ -49,10 +52,10 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, async (jwtPayload, done
 //   }
 // }));
 
-exports.verifyUser = passport.authenticate('jwt', { session: false });
+exports.verifyUser = passport.authenticate("jwt", { session: false });
 exports.verifyAdmin = (req, res, next) => {
   if (!req.user.admin) {
-    const err = new Error('You are not authorized to perform this operation!');
+    const err = new Error("You are not authorized to perform this operation!");
     err.status = 401;
     next(err);
     return;
---
+++ routes/leaderRouter.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const Leaders = require("../models/leaders");
+const authenticate = require("../authenticate");
+const cors = require("./cors");
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(express.json());
+
+leaderRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, async (req, res, next) => {
+    try {
+      const leaders = await Leaders.find(req.query);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json(leaders);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    async (req, res, next) => {
+      try {
+        const leader = await Leaders.create(req.body);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(leader);
+      } catch (err) {
+        next(err);
+      }
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res) => {
+      res.statusCode = 403;
+      res.end("PUT operation not supported on /leaders");
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    async (req, res, next) => {
+      try {
+        const resp = await Leaders.remove({});
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(resp);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+
+leaderRouter
+  .route("/:leaderId")
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, async (req, res, next) => {
+    try {
+      const leader = await Leaders.findById(req.params.leaderId);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json(leader);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res) => {
+      res.statusCode = 403;
+      res.end(
+        `POST operation not supported on /leaders/${req.params.leaderId}`
+      );
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    async (req, res, next) => {
+      try {
+        const leader = await Leaders.findByIdAndUpdate(
+          req.params.leaderId,
+          { $set: req.body },
+          { new: true }
+        );
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(leader);
+      } catch (err) {
+        next(err);
+      }
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    async (req, res, next) => {
+      try {
+        const resp = await Leaders.findByIdAndRemove(req.params.leaderId);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(resp);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+
+module.exports = leaderRouter;
